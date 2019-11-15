b9dce17e51c021586252ccb357f9d489a9af1de5
masknugget <54693275+masknugget@users.noreply.github.com>
2019-11-15 19:40:20 +0800

    Update flatten.py

--- algorithms/arrays/flatten.py
+++ algorithms/arrays/flatten.py
@@ -11,10 +11,10 @@ def flatten(input_arr, output_arr=None):
     if output_arr is None:
         output_arr = []
     for ele in input_arr:
-        if isinstance(ele, Iterable):
+        if isinstance(ele, Iterable):                             # 这个操作六六六
             flatten(ele, output_arr)    #tail-recursion
         else:
-            output_arr.append(ele)      #produce the result
+            output_arr.append(ele)      #produce the result       # ++
     return output_arr
 
 
@@ -26,6 +26,6 @@ def flatten_iter(iterable):
     """
     for element in iterable:
         if isinstance(element, Iterable):
-            yield from flatten_iter(element)    
+            yield from flatten_iter(element)                 # yield from 
         else:
             yield element
