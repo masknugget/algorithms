14e4809a091cb7e7f785dad0db7d1bd43dd1f230
masknugget <54693275+masknugget@users.noreply.github.com>
2019-11-15 19:32:22 +0800

    Update delete_nth.py

--- algorithms/arrays/delete_nth.py
+++ algorithms/arrays/delete_nth.py
@@ -5,6 +5,9 @@ that contains each number of the list at most N times without reordering.
 For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], 
 drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, 
 which leads to [1,2,3,1,2,3]
+
+在一个list中限制频率为n的，出现的频率
+
 """
 import collections
 
