642037316e698fe453ff2c4ff82dafa96a55b664
masknugget <54693275+masknugget@users.noreply.github.com>
2019-11-20 16:58:28 +0800

    Update limit.py

--- algorithms/arrays/limit.py
+++ algorithms/arrays/limit.py
@@ -10,7 +10,38 @@ If array, Min, Max value was given, it returns array that contains values of
 ex) limit([1,2,3,4,5], None, 3) = [1,2,3]
 
 Complexity = O(n)
+
+这个编程方式倒是不错
+
+def limit(text, store = ''):
+    store += text
+    print(store)      # 这个store不会增加
+    return text      
+
+
+def limit1(text, store = []):
+    store.append(text)
+    print(store)      # 不过这个会增加
+    return text
+    
+store = []
+def limit2(text):
+    store.append(text)
+    print(store)
+    return text
+    
+
+def limit1(text, store = []):       # 这种方式并不像
+    store.append(text)
+    print(store)
+    store = []
+    return text
+    
+    
+
 """
+
+
 
 # tl:dr -- array slicing by value
 def limit(arr, min_lim=None, max_lim=None):
